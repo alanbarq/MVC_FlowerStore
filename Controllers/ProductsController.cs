3a71230454df63b6fcb16f2b64c9d5ba6dfc679f
alanbarq <eabarlandas05@gmail.com>
2022-02-03 16:41:20 -0600

    MVC Application

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCFlowerApp.Models;
+
+namespace MVCFlowerApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        List<Tuple<tbl_products, int>> cart = new List<Tuple<tbl_products, int>>();
+        private FlowerAppEntities db = new FlowerAppEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var tbl_products = db.tbl_products.Include(t => t.tbl_categories);
+            return View(tbl_products.ToList());
+        }
+        
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_products tbl_products = db.tbl_products.Find(id);
+            if (tbl_products == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_products);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID_Category = new SelectList(db.tbl_categories, "ID_category", "category");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Code_product,Name,Price,Description,ID_Category,ID")] tbl_products tbl_products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbl_products.Add(tbl_products);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_Category = new SelectList(db.tbl_categories, "ID_category", "category", tbl_products.ID_Category);
+            return View(tbl_products);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_products tbl_products = db.tbl_products.Find(id);
+            if (tbl_products == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_Category = new SelectList(db.tbl_categories, "ID_category", "category", tbl_products.ID_Category);
+            return View(tbl_products);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Code_product,Name,Price,Description,ID_Category,ID")] tbl_products tbl_products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_products).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID_Category = new SelectList(db.tbl_categories, "ID_category", "category", tbl_products.ID_Category);
+            return View(tbl_products);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_products tbl_products = db.tbl_products.Find(id);
+            if (tbl_products == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_products);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbl_products tbl_products = db.tbl_products.Find(id);
+            db.tbl_products.Remove(tbl_products);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Customer(string username)
+        {
+            ViewBag.Message = username;
+            return View();
+        }
+
+        public ActionResult ShowProducts(string category)
+        {
+            ViewBag.Message = category;
+            var tbl_products = db.tbl_products.Include(t => t.tbl_categories);
+            return View(tbl_products.ToList());
+
+        }
+
+        public void SumToCart(tbl_products tbl_product, int quantity)
+        {
+            Tuple<tbl_products, int> product_selected = new Tuple<tbl_products,int>(tbl_product, quantity);
+            this.cart.Add(product_selected);
+        }
+
+
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MVCFlowerApp.Models;
+
+namespace MVCFlowerApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private FlowerAppEntities db = new FlowerAppEntities();
+
+        // GET: User
+        public ActionResult Index()
+        {
+            return View(db.tbl_users.ToList());
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_users tbl_users = db.tbl_users.Find(id);
+            if (tbl_users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_users);
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "username,name,lastname,password,address,mail,telephone,ID_user")] tbl_users tbl_users)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbl_users.Add(tbl_users);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tbl_users);
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_users tbl_users = db.tbl_users.Find(id);
+            if (tbl_users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_users);
+        }
+
+        public ActionResult MainMenu(string message = "")
+        {
+            ViewBag.Message = message;
+            return View();
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "username,name,lastname,password,address,mail,telephone,ID_user")] tbl_users tbl_users)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_users).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tbl_users);
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_users tbl_users = db.tbl_users.Find(id);
+            if (tbl_users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_users);
+        }
+
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbl_users tbl_users = db.tbl_users.Find(id);
+            db.tbl_users.Remove(tbl_users);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Login(string username,string password)
+        {
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                if (username == "aa" && password == "aa")
+                {
+                    return RedirectToAction("Index", "Products");
+                }
+                tbl_users user = db.tbl_users.FirstOrDefault(e => e.username == username);
+
+                if (user != null)
+                {
+                    if (user.password == password)
+                    {
+                        FormsAuthentication.SetAuthCookie(user.username, true);
+                        return RedirectToAction("Customer", "Products",new { username = user.username });
+                    }
+                    else
+                    {
+                        return RedirectToAction("MainMenu", "User", new { message = "The password was incorrect, please try again" });
+
+                    }
+
+                }
+                else
+                {
+                    return RedirectToAction("MainMenu", "User", new { message = "The user does not exist, please try again" });
+                }
+
+                }
+            else
+                {
+                    return RedirectToAction("MainMenu", "User", new { message = "Fill the username and password spaces" });
+                } 
+            
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
